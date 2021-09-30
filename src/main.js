dbb8b6695105b8b5266e8286ff5ae55c554597ae
angela <angela.valdebenito.v@gmail.com>
2021-09-30 00:08:55 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Firebase.initializeApp(firebaseConfig);
+import Firebase from "firebase"
+import { firebaseConfig } from "../firebaseConfig";
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>Formulario</h3>
+        <form @submit.prevent="validarForm">
+            <label for="correo">Correo: </label>
+            <input v-model="email" id="correo" type="text" required>
+            <label for="contraseña">Contraseña: </label>
+            <input v-model="password" placeholder="introduce tu contraseña" id="contraseña" type="password" required>
+            <button type="submit">Validar</button>
+        </form>
+    </div>
+</template>
+<script>
+import Firebase from 'firebase';
+export default {
+    data: () => ({
+        email: "",
+        password: ""
+    }),
+    methods:{
+        validarForm(){
+            Firebase.auth()
+            .signInWithEmailAndPassword(this.email, this.password)
+            .then(response => {
+            console.log(response)
+            })
+            .catch (response => {
+            console.error(response)
+            })
+        }
+    }
+}
+</script>
